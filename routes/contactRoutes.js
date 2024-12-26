c6b3750aa037f4d01a78fdfe0211015a3d6705ec
Movie_Website <sahilkumaraz@gmail.com>
2024-12-26 18:20:24 +0530

    second commit

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,23 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.route("/").get((req,res)=>{
-    res.status(200).json({massage:"get all contect"});
-});
+router.route("/")
+    .get((req, res) => {
+        res.status(200).json({ message: "Get all contacts" });
+    })
+    .post((req, res) => {
+        res.status(200).json({ message: "Create contact" });
+    });
+
+router.route("/:id")
+    .get((req, res) => {
+        res.status(200).json({ message: `Get contact for ${req.params.id}` });
+    })
+    .put((req, res) => {
+        res.status(200).json({ message: `Update contact for ${req.params.id}` });
+    })
+    .delete((req, res) => {
+        res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+    });
+
 module.exports = router;
